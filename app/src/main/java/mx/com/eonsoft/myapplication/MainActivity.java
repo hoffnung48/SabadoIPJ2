7aeb8b65cc2fea94b154fb2a85cf2d7710fdc6c4
ivan.perez <ivan.perez@globant.com>
2018-02-16 12:16:14 -0600

    first commit

---
+++ app/src/main/java/mx/com/eonsoft/myapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package mx.com.eonsoft.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button boton1,boton2;
+    private TextView texto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        boton1 = findViewById(R.id.button);
+        boton2 = findViewById(R.id.button2);
+        texto = findViewById(R.id.textView);
+
+
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                texto.setText("Hola GitHub");
+            }
+        });
+
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                texto.setText("Diplomado Android");
+
+            }
+        });
+    }
+}
